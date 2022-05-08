02ad6e87547d9374359253f89facaf1d637d8d72
Mjb222 <94459374+Mjb222@users.noreply.github.com>
2022-05-08 19:08:38 +0200

    added github link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        <a href="https://github.com/Mjb222/r-weather" target="_blank">
+          {" "}
+          Open-source project,{" "}
+        </a>
+        coded by Marium Bajwa
+      </footer>
+    </div>
+  );
 }
