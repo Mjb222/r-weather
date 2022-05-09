63f07e29d107946f5f65e4342ee2efa1bfc407ec
Mjb222 <94459374+Mjb222@users.noreply.github.com>
2022-05-09 11:57:29 +0200

    added bootsrap

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        <a href="https://github.com/Mjb222/r-weather" target="_blank">
-          {" "}
-          Open-source project,{" "}
-        </a>
-        coded by Marium Bajwa
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          <a href="https://github.com/Mjb222/r-weather" target="_blank">
+            {" "}
+            Open-source project,{" "}
+          </a>
+          coded by Marium Bajwa
+        </footer>
+      </div>
     </div>
   );
 }
