d9de548bd4b2592e7796c6cfc661b76a917afb99
Mjb222 <94459374+Mjb222@users.noreply.github.com>
2022-05-15 12:11:15 +0200

    footer changes

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ export default function App() {
         <Weather defaultCity="New York" />
       </div>
       <footer>
-        Project coded by Marium Bajwa and is
+        Project coded by Marium Bajwa,{" "}
         <a href="https://github.com/Mjb222/r-weather" target="_blank">
           {" "}
-          Open-source project on GitHub,{" "}
+          open-source project on GitHub,{" "}
         </a>
         and
         <a href="https://luminous-capybara-7b0b7c.netlify.app/" target="_blank">
